20f3cb777c3d0a8884411a8cf273c6e666a831f4
Matheus <math.a.monteiro@gmail.com>
2023-04-26 10:33:13 -0300

    adding deppendencies

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require("cors");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var productsRouter = require("./routes/products");
 
 var app = express();
 
 // view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -19,8 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
