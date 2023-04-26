983974529ab219eb2e6afbfb0cb03c09bafe59fc
Matheus <math.a.monteiro@gmail.com>
2023-04-26 13:49:49 -0300

    productListPage basic layout

---
+++ frontend/src/components/ProductListPage/ProductCard/ProductCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: ['product']
+}
+</script>
+
+<template>
+  <div class="cardWrapper">
+    <p>
+      Nome: <span>{{ product.name }}</span>
+    </p>
+    <p>
+      Categoria: <span>{{ product.category }}</span>
+    </p>
+    <p>
+      Preço: <span>{{ product.price }}</span>
+    </p>
+    <p>
+      Quantidade: <span>{{ product.quantity }}</span>
+    </p>
+  </div>
+</template>
+
+<style>
+.cardWrapper {
+  border: solid 1px black;
+  padding: 1rem;
+  height: 200px;
+}
+</style>
